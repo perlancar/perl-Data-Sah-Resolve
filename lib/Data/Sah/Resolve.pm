02082fbb84a66b2a8cf77b55cadafec8e8392598
perlancar (@pc-home) <perlancar@gmail.com>
2016-07-18 15:21:23 +0700

    WIP

--- lib/Data/Sah/Resolve.pm
+++ lib/Data/Sah/Resolve.pm
@@ -8,11 +8,38 @@ use strict;
 use warnings;
 
 use Exporter qw(import);
-our @EXPORT_OK = qw(resolve_sah_schema);
+our @EXPORT_OK = qw(resolve_schema);
+
+sub _resolve {
+    my ($opts, $type, $clsets) = @_;
+
+    (my $typemod_pm = "Data/Sah/Type/$type.pm") =~ s!::!/!g;
+    eval { require $typemod_pm; 1 };
+    # already a builtin-type, so just return the schema's type name & clause set
+    return [$type, $clsets] unless $@;
+
+    # not a type, try a schema under Sah::Schema
+    my $schmod = "Sah::Schema::$type";
+    (my $schmod_pm = "$schmod.pm") =~ s!::!/!g;
+    eval { require $schmod_pm; 1 };
+    die "Not a known type/schema name '$type'" if $@;
+    no strict 'refs';
+    my $sch2 = ${"$schmod\::schema"};
+    die "BUG: Schema module $schmod doesn't contain \$schema" unless $sch2;
+    push @$clsets, $sch2->[1];
+    _resolve($opts, $sch2->[0], $clsets);
+}
 
 sub resolve_sah_schema {
-use Data::Sah::Normalize qw(normalize_schema);
+    my $opts = ref($_[0]) eq 'HASH' ? shift : {};
+    my $sch = shift;
 
+    unless ($opts->{schema_is_normalized}) {
+        require Data::Sah::Normalize;
+        $sch =  Data::Sah::Normalize::normalize_schema($sch);
+    }
+
+    _resolve($opts, $sch->[0], [$sch->[1]]);
 }
 
 1;
@@ -20,18 +47,18 @@ use Data::Sah::Normalize qw(normalize_schema);
 
 =head1 SYNOPSIS
 
- use Data::Sah::Resolve qw(resolve_sah_schema);
+ use Data::Sah::Resolve qw(resolve_schema);
 
- my $sch = resolve_sah_schema("int");
+ my $sch = resolve_schema("int");
  # => ["int", {}, {}]
 
- my $sch = resolve_sah_schema("posint*");
+ my $sch = resolve_schema("posint*");
  # => ["int", {req=>1, min=>0}, {}]
 
- my $sch = resolve_sah_schema([posint => div_by => 3]);
+ my $sch = resolve_schema([posint => div_by => 3]);
  # => ["int", {min=>0, div_by=>3}, {}]
 
- my $sch = resolve_sah_schema([array => of=>"posint*"]);
+ my $sch = resolve_schema([array => of=>"posint*"]);
  # => ["array", {of=>["int", {req=>1, min=>0}, {}]}, {}]
 
 
@@ -40,7 +67,7 @@ use Data::Sah::Normalize qw(normalize_schema);
 
 =head1 FUNCTIONS
 
-=head2 resolve_sah_schema([ \%opts, ] $sch) => sch
+=head2 resolve_schema([ \%opts, ] $sch) => sch
 
 Extract all subschemas found inside Sah schema C<$sch>. Schema will be
 normalized first, then schemas from all clauses which contains subschemas will
