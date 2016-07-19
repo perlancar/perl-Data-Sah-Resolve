d60d38d05f2fa847b83165f55a3c69b04388680d
perlancar (@pc-home) <perlancar@gmail.com>
2016-07-19 15:52:19 +0700

    Finish first implementation

--- lib/Data/Sah/Resolve.pm
+++ lib/Data/Sah/Resolve.pm
@@ -11,7 +11,11 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(resolve_schema);
 
 sub _resolve {
-    my ($opts, $type, $clsets) = @_;
+    my ($opts, $type, $clsets, $seen) = @_;
+
+    die "Recursive schema definition: ".join(" -> ", @$seen, $type)
+        if grep { $type eq $_ } @$seen;
+    push @$seen, $type;
 
     (my $typemod_pm = "Data/Sah/Type/$type.pm") =~ s!::!/!g;
     eval { require $typemod_pm; 1 };
@@ -26,11 +30,11 @@ sub _resolve {
     no strict 'refs';
     my $sch2 = ${"$schmod\::schema"};
     die "BUG: Schema module $schmod doesn't contain \$schema" unless $sch2;
-    push @$clsets, $sch2->[1];
-    _resolve($opts, $sch2->[0], $clsets);
+    unshift @$clsets, $sch2->[1];
+    _resolve($opts, $sch2->[0], $clsets, $seen);
 }
 
-sub resolve_sah_schema {
+sub resolve_schema {
     my $opts = ref($_[0]) eq 'HASH' ? shift : {};
     my $sch = shift;
 
@@ -38,8 +42,58 @@ sub resolve_sah_schema {
         require Data::Sah::Normalize;
         $sch =  Data::Sah::Normalize::normalize_schema($sch);
     }
+    $opts->{merge_clause_sets} //= 1;
 
-    _resolve($opts, $sch->[0], [$sch->[1]]);
+    my $res = _resolve($opts, $sch->[0], keys(%{$sch->[1]}) ? [$sch->[1]] : [], []);
+
+  MERGE:
+    {
+        last unless $opts->{merge_clause_sets};
+        last if @{ $res->[1] } < 2;
+
+        my @clsets = (shift @{ $res->[1] });
+        for my $clset (@{ $res->[1] }) {
+            my $has_merge_mode_keys;
+            for (keys %$clset) {
+                if (/\Amerge\./) {
+                    $has_merge_mode_keys = 1;
+                    last;
+                }
+            }
+            if ($has_merge_mode_keys) {
+                state $merger = do {
+                    require Data::ModeMerge;
+                    my $mm = Data::ModeMerge->new(config => {
+                        recurse_array => 1,
+                    });
+                    $mm->modes->{NORMAL}  ->prefix   ('merge.normal.');
+                    $mm->modes->{NORMAL}  ->prefix_re(qr/\Amerge\.normal\./);
+                    $mm->modes->{ADD}     ->prefix   ('merge.add.');
+                    $mm->modes->{ADD}     ->prefix_re(qr/\Amerge\.add\./);
+                    $mm->modes->{CONCAT}  ->prefix   ('merge.concat.');
+                    $mm->modes->{CONCAT}  ->prefix_re(qr/\Amerge\.concat\./);
+                    $mm->modes->{SUBTRACT}->prefix   ('merge.subtract.');
+                    $mm->modes->{SUBTRACT}->prefix_re(qr/\Amerge\.subtract\./);
+                    $mm->modes->{DELETE}  ->prefix   ('merge.delete.');
+                    $mm->modes->{DELETE}  ->prefix_re(qr/\Amerge\.delete\./);
+                    $mm->modes->{KEEP}    ->prefix   ('merge.keep.');
+                    $mm->modes->{KEEP}    ->prefix_re(qr/\Amerge\.keep\./);
+                    $mm;
+                };
+                my $merge_res = $merger->merge($clsets[-1], $clset);
+                unless ($merge_res->{success}) {
+                    die "Can't merge clause set: $merge_res->{error}";
+                }
+                $clsets[-1] = $merge_res->{result};
+            } else {
+                push @clsets, $clset;
+            }
+        }
+
+        $res->[1] = \@clsets;
+    }
+
+    $res;
 }
 
 1;
@@ -50,7 +104,7 @@ sub resolve_sah_schema {
  use Data::Sah::Resolve qw(resolve_schema);
 
  my $sch = resolve_schema("int");
- # => ["int", {}, {}]
+ # => ["int", []]
 
  my $sch = resolve_schema("posint*");
  # => ["int", {req=>1, min=>0}, {}]
@@ -81,6 +135,8 @@ Known options:
 
 When set to true, function will skip normalizing schema and assume input schema
 is normalized.
+
+=item * merge_clause_sets => bool (default: 1)
 
 =back
 
